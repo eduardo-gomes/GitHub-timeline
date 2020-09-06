2ba6f8a19c8760e74eaa198323b146e2a285e2ee
Eduardo M Gomes <eduardo.2001.06@gmail.com>
2020-09-06 02:05:24 -0300

    fix cp path, more style fix old style

--- js/rateToHTML.ts
+++ js/rateToHTML.ts
@@ -1,4 +1,11 @@
 function rateFromHeader(header: ResponseHeader) {
+	function getToHtml(remaining: number, limit: number, reset: Date){
+		return(`
+			<p>Remaining API requests: ${remaining}</p>
+			<p>it will reset to ${limit} at: ${reset.toLocaleString()}</p>
+		`
+		);
+	}
 	var rateLimit = header["x-ratelimit-limit"], rateRemaining = header["x-ratelimit-remaining"], rateReset = header["x-ratelimit-reset"];
 	console.log({ rateLimit, rateRemaining, rateReset })
 	if (rateRemaining != undefined && rateLimit != undefined && rateReset != undefined) {
@@ -8,11 +15,13 @@ function rateFromHeader(header: ResponseHeader) {
 			alert(`Remaining API requests(each search uses 2): ${rateRemainingNum}\nThis will reset to ${rateLimitNum} at ${rateResetDate.toLocaleString()}`)
 		var remainingHTMLElement = document.getElementById('remaining-requests');
 		if (remainingHTMLElement == null) {//create element
-			remainingHTMLElement = document.createElement('p');
+			remainingHTMLElement = document.createElement('div');
+			remainingHTMLElement.id = "remaining-requests";
 			const searchDiv = document.getElementById('search') as HTMLElement;
 			searchDiv.appendChild(remainingHTMLElement);
+			alert('remaining-requests created');
 		}
-		remainingHTMLElement.innerHTML = `Remaining API requests: ${rateRemainingNum}`;
+		remainingHTMLElement.innerHTML = getToHtml(rateRemainingNum, rateLimitNum, rateResetDate);
 	}
 }
 
